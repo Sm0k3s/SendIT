d296662dba34f893120a6fecadb038d502004d8b
sm0k3s <obaga4@gmail.com>
2018-11-02 10:22:16 +0300

    Finishes cancel button and modal to edit delivery [Finishes #161597586, Finishes #161598056, Finishes #161598382]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,4 +1,4 @@
-// Variables
+// letiables
 
 //Event Listeners
 eventListeners();
@@ -15,4 +15,26 @@ function calcPrice(e){
   const price = document.querySelector('.weight').value;
   let ne = parseInt(price) * 2
   console.log(place, ne);
+}
+
+// Modal
+let modal = document.getElementById('edit');
+
+let btn = document.querySelector(".edit");
+
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
