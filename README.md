76a48ebac3231989fb7784c0a14786afcb5ab98f
sm0k3s <obaga4@gmail.com>
2018-11-06 06:13:32 +0300

    [Feature #161741643] Creates user model and method to create a user

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Open Source Love svg2](https://badges.frapsoft.com/os/v2/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+[![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+
 # SendIT
-### SendIT is a courier service that helps users deliver parcels to different destinations. SendIT 
-### provides courier quotes based on weight categories. 
+ SendIT is a courier service that helps users deliver parcels to different destinations. SendIT provides courier quotes based on weight categories.
--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,5 +1,31 @@
-# class Global():
-#     def save():
-#
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User():
+    """
+    A model for manipulating data for the users
+    """
+    Access = {
+        2: 'admin',
+        1 : 'user'
+    }
+
+    database = {}
+    _id = 1
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    @classmethod
+    def create_user(cls, username, email, password):
+        user = {
+            "id": cls._id,
+            "username": username,
+            "password": generate_password_hash(password),
+            "email":email,
+            "role": cls.Access[2]
+        }
+        cls.database[cls._id] = user
+        cls._id += 1
