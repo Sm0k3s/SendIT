1428dc5305de24161689514f580f0658501d6db0
sm0k3s <obaga4@gmail.com>
2018-11-07 10:01:39 +0300

    [Chore #161744429] Adds tests to get specific order and cancel order

--- app/tests/v1/test_parcel.py
+++ app/tests/v1/test_parcel.py
@@ -13,7 +13,15 @@ class TestParcel(BaseTest):
         self.assertEqual(resp.status_code, 200)
 
     def test_cancel_parcel(self):
-        pass
+        """Cancels the delivery order that matches the id provided"""
+        #creates a parcel order first
+        self.client.post('/api/v1/parcels', data=json.dumps(self.order), content_type='application/json')
+        #change cancels the parcel order
+        resp = self.client.put('/api/1/cancel', data=json.dumps(), content_type='application/json')
+        self.assertEqual(resp.status_code, 201)
 
     def test_get_specific_parcel(self):
-        pass
+        """Gets a parcel that matches the id provided"""
+        self.client.post('/api/v1/parcels', data=json.dumps(self.order), content_type='application/json')
+        resp = self.client.get('/api/v1/parcels/1')
+        self.assertEqual(resp.status_code, 200)
