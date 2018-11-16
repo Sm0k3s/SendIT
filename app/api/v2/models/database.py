68a1c016f88c6691d7cdce18ae6f5216097997dd
sm0k3s <obaga4@gmail.com>
2018-11-16 16:58:17 +0300

    [Finishes #161865743] Finishes setting up the database environment

--- app/api/v2/models/database.py
+++ app/api/v2/models/database.py
@@ -1,5 +1,23 @@
+"""Module for the database model"""
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 class Database():
+    """
+    Database model
+    """
+    @staticmethod
+    def initialize():
+        """Method to start the connection with the database"""
+        conn = psycopg2.connect(app.config['DATABASE_URI'])
+        cur = conn.cursor(cursor_factory=RealDictCursor)
 
-    
+    @staticmethod
+    def create_all():
+        """creates all tables"""
+        pass
+
+    def drop_all():
+        """drops all tables"""
+        pass
