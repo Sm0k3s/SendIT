4b0acc5c90674b61f516011994d9ff6fc6d552fa
sm0k3s <obaga4@gmail.com>
2018-11-17 08:35:17 +0300

    [Chore #161993193] check travis errors

--- .travis.sh
+++ .travis.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+export TEST_DATABASEURI="dbname='sendit_test' user='smokes' password='smokey' host='localhost'"
 export DATABASEURI="dbname='sendit_test' user='smokes' password='smokey' host='localhost'"
 export JWT_SECRET_KEY="shh_shhuutt_uup"
